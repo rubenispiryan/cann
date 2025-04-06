6ae65e23db0bc26378a4dcfcb55b4a79b1a7a027
rubenispiryan <ruben.ispir@gmail.com>
2025-04-06 12:51:59 -0400

    Add Weight Initialization Methods

---
+++ rand_distr.c
@@ -0,0 +1,39 @@
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "rand_distr.h"
+
+static double rand_uniform(double left, double right) {
+    assert(left <= right);
+    double uniform_number = rand();
+    return (right - left) * (uniform_number / (double) RAND_MAX) + left;
+}
+
+static double rand_normal(double mean, double std) {
+    double u1 = rand() / (double) RAND_MAX;
+    double u2 = rand() / (double) RAND_MAX;
+    double z0 = sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
+    return z0 * std + mean;
+}
+
+double uniform_xavier(int n_input, int n_output) {
+    double range = sqrt(6.0 / (n_input + n_output));
+    return rand_uniform(-range, range);
+}
+
+double normal_xavier(int n_input, int n_output) {
+    double sigma = sqrt(2.0 / (n_input + n_output));
+    return rand_normal(0, sigma);
+}
+
+double uniform_he(int n_input, int n_output) {
+    double left = -sqrt(6.0 / n_input);
+    double right = sqrt(6.0 / n_output);
+    return rand_uniform(left, right);
+}
+
+double normal_he(int n_input, int n_output) {
+    double sigma = sqrt(2.0 / n_input);
+    return rand_normal(0, sigma);
+}
---
+++ rand_distr.h
@@ -0,0 +1,12 @@
+#ifndef _RAND_DISTR_
+#define _RAND_DISTR_
+
+double uniform_xavier(int n_input, int n_output);
+
+double normal_xavier(int n_input, int n_output);
+
+double uniform_he(int n_input, int n_output);
+
+double normal_he(int n_input, int n_output);
+
+#endif
